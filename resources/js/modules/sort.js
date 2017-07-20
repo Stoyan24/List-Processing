09b10ae8cafe545685a33617ca57f334caf28d1a
ISpectruM <ispectrumcode@gmail.com>
2017-07-20 11:35:26 +0300

    Added sort functionality.

--- resources/js/modules/sort.js
+++ resources/js/modules/sort.js
@@ -1,3 +1,9 @@
 module.exports = function (array,commands) {
+
+  if(commands.length > 0){
+    return 'Error: invalid command parameters';
+  }
+
+  array.sort((a,b)=> a.toLowerCase().localeCompare(b.toLowerCase()));
   return true
 };
