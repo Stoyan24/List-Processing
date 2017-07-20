6876e204c4cd644a3faad5babcadb80889e5d709
Stoyan24 <stoiansmilqnov24@gmail.com>
2017-07-20 15:31:33 +0300

    Fix again deleteItem

--- resources/js/modules/deleteItem.js
+++ resources/js/modules/deleteItem.js
@@ -1,18 +1,17 @@
-module.exports = function (array, commands) {
-
-    let tokens = commands.map(el => Number(el));
+module.exports = function (array, tokens) {
 
     if (tokens.length === 0 || tokens.length > 1) {
         return 'Error: invalid command parameters';
     }
+
+    if (parseInt(tokens[0],10) != tokens[0]) {
+        return `Error: invalid index ${tokens[0]}`;
+    }
+
     if (tokens[0] < 0 || tokens[0] > array.length - 1) {
         return `Error: invalid index ${tokens[0]}`;
     }
-    if(isNaN(tokens)){
-        return 'Error: invalid command parameter';
-    }
-    
-    array.splice(tokens[0], 1);
-    return true
 
+    array.splice(tokens[0], 1);
+    return true;
 };
