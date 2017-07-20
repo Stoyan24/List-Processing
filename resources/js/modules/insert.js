06018055bc2ca8c2deb45b5ad2e749ac9c4a7a7c
nstalev <stalev_a@abv.bg>
2017-07-20 09:26:41 +0300

    adding functions - insert and roll

--- resources/js/modules/insert.js
+++ resources/js/modules/insert.js
@@ -1,3 +1,19 @@
 module.exports = function (array,commands) {
-  return true
+
+    if (commands.length  <= 1 ||  commands.length > 2) {
+        return 'Error: invalid command parameters';
+    }
+
+    let index  = Number(commands[0]);
+
+    let str = commands[1];
+
+
+    if(index < 0 || !Number.isInteger(index)){
+        return `Error: invalid index ${index}`;
+    }
+
+    array.splice(index, 0, str);
+
+    return true
 };
--- resources/js/modules/roll.js
+++ resources/js/modules/roll.js
@@ -1,3 +1,22 @@
 module.exports = function (array,commands) {
-  return true
+
+    if (commands.length  === 0 ||  commands.length > 1) {
+        return 'Error: invalid command parameters';
+    }
+
+    let command = '' + commands;
+
+    if(command ==='right'){
+
+        let lastString = array.pop();
+        array.unshift(lastString);
+    }
+
+    if(command === 'left'){
+        let firstString = array.shift();
+        array.push(firstString);
+    }
+
+    return true
+
 };
