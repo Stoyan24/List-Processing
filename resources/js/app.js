24d2e449da1b577ca9889d3b5149489c8a9aaf5a
ISpectruM <ispectrumcode@gmail.com>
2017-07-20 12:39:31 +0300

    Correction in execCommand function.

--- resources/js/app.js
+++ resources/js/app.js
@@ -70,11 +70,8 @@ function initializeListProcessor() {
 
   function execCommand(func, array, commandLineTokens) {
     let result = func(array,commandLineTokens);
-    if(Number.isInteger(result)){
-      display(result);
-    }
-    else if (result === true) {
-      display(array.join(', '))
+    if (result === true) {
+      display(array.join(', '));
     }
     else {
       display(result);
